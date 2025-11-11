6f8eb9ac470e0ea8ee387627a71221c23ff7625e
Ramiro Kalinoski NB <ramikalinoski@gmail.com>
2025-11-11 12:57:29 -0300

    Conexion a backend con URL IPv4 personalizada

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -33,7 +33,8 @@ export const COLORS = {
 
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: 'http://10.0.2.2:8080/api/v1',
+  //BASE_URL: 'http://10.0.2.2:8080/api/v1',
+  BASE_URL: 'http://192.168.0.164:8080/api/v1',
   TIMEOUT: 30000,
   USE_MOCK: false, // Set to false to use real backend
 };
